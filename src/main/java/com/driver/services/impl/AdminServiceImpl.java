2162b21a20ec5fa98458f60fc84133b4ea1db1ba
Talib Malik <mohammedtalib5@gmail.com>
2023-04-04 04:39:47 +0530

    admin-reg

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,81 +1,5 @@
 package com.driver.services.impl;
 
-//import com.driver.model.Admin;
-//import com.driver.model.Country;
-//import com.driver.model.CountryName;
-//import com.driver.model.ServiceProvider;
-//import com.driver.repository.AdminRepository;
-//import com.driver.repository.CountryRepository;
-//import com.driver.repository.ServiceProviderRepository;
-//import com.driver.services.AdminService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class AdminServiceImpl implements AdminService {
-//    @Autowired
-//    AdminRepository adminRepository1;
-//
-//    @Autowired
-//    ServiceProviderRepository serviceProviderRepository1;
-//
-//    @Autowired
-//    CountryRepository countryRepository1;
-//
-//    @Override
-//    public Admin register(String username, String password) {
-//        Admin admin = new Admin(username, password);
-//        return  adminRepository1.save(admin);
-//    }
-//
-//    @Override
-//    public Admin addServiceProvider(int adminId, String providerName) {
-//         Admin admin = adminRepository1.findById(adminId).get();
-//         ServiceProvider serviceProvider = new ServiceProvider(providerName);
-//         serviceProvider.setAdmin(admin);
-//         admin.getServiceProviders().add(serviceProvider);
-//         adminRepository1.save(admin);
-//         return admin;
-//    }
-//
-//    @Override
-//    public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-//        ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
-//         Country country = new Country();
-//         CountryName cname;
-//         String code;
-//        if(countryName.equalsIgnoreCase("IND")){   //either use ignorecase which just ignore upper or lower case
-//            cname = CountryName.IND;                      // or do the classic thing ie make incoming parameter to uppercase using String.toUpperCase method
-//            code = CountryName.IND.toCode();
-//
-//        }
-//        else if(countryName.equalsIgnoreCase("USA")){
-//            cname = CountryName.USA;
-//            code = CountryName.USA.toCode();
-//        }
-//        else if(countryName.equalsIgnoreCase("AUS")){
-//            cname = CountryName.AUS;
-//            code = CountryName.AUS.toCode();
-//        }
-//        else if(countryName.equalsIgnoreCase("CHI")){
-//            cname = CountryName.CHI;
-//            code = CountryName.CHI.toCode();
-//        }
-//        else if(countryName.equalsIgnoreCase("JPN")){
-//            cname = CountryName.JPN;
-//            code = CountryName.JPN.toCode();
-//        }
-//        else{
-//            throw new Exception("Country not found");
-//        }
-//        country.setCountryName(cname);
-//        country.setCode(code);
-//        country.setServiceProvider(serviceProvider);
-//        serviceProvider.getCountryList().add(country);
-//        serviceProviderRepository1.save(serviceProvider);
-//        return serviceProvider;
-//    }
-//}
 import com.driver.model.Admin;
 import com.driver.model.Country;
 import com.driver.model.CountryName;
@@ -100,66 +24,143 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
-        Admin admin = new Admin(username,password);
-
-        adminRepository1.save(admin);
-        return admin;
+        Admin admin = new Admin(username, password);
+         adminRepository1.save(admin);
+         return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
-        Admin admin = adminRepository1.findById(adminId).get();
-        ServiceProvider serviceProvider = new ServiceProvider(providerName);
-
-        serviceProvider.setAdmin(admin);
-        admin.getServiceProviders().add(serviceProvider);
-
-        adminRepository1.save(admin);
-        return admin;
+         Admin admin = adminRepository1.findById(adminId).get();
+         ServiceProvider serviceProvider = new ServiceProvider(providerName);
+         serviceProvider.setAdmin(admin);
+         admin.getServiceProviders().add(serviceProvider);
+         adminRepository1.save(admin);
+         return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-        Country country = new Country();
         ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+         Country country = new Country();
+         CountryName cname;
+         String code;
+        if(countryName.equalsIgnoreCase("IND")){   //either use ignorecase which just ignore upper or lower case
+            cname = CountryName.IND;                      // or do the classic thing ie make incoming parameter to uppercase using String.toUpperCase method
+            code = CountryName.IND.toCode();
 
-        if(!isValidCountryName(countryName)){
+        }
+        else if(countryName.equalsIgnoreCase("USA")){
+            cname = CountryName.USA;
+            code = CountryName.USA.toCode();
+        }
+        else if(countryName.equalsIgnoreCase("AUS")){
+            cname = CountryName.AUS;
+            code = CountryName.AUS.toCode();
+        }
+        else if(countryName.equalsIgnoreCase("CHI")){
+            cname = CountryName.CHI;
+            code = CountryName.CHI.toCode();
+        }
+        else if(countryName.equalsIgnoreCase("JPN")){
+            cname = CountryName.JPN;
+            code = CountryName.JPN.toCode();
+        }
+        else{
             throw new Exception("Country not found");
         }
-
-        if (countryName.equalsIgnoreCase("IND")){
-            country.setCountryName(CountryName.IND);
-            country.setCode(CountryName.IND.toCode());
-        }
-        if (countryName.equalsIgnoreCase("USA")){
-            country.setCountryName(CountryName.USA);
-            country.setCode(CountryName.USA.toCode());
-        }
-        if (countryName.equalsIgnoreCase("AUS")){
-            country.setCountryName(CountryName.AUS);
-            country.setCode(CountryName.AUS.toCode());
-        }
-        if (countryName.equalsIgnoreCase("JPN")){
-            country.setCountryName(CountryName.JPN);
-            country.setCode(CountryName.JPN.toCode());
-        }
-        if (countryName.equalsIgnoreCase("CHI")){
-            country.setCountryName(CountryName.CHI);
-            country.setCode(CountryName.CHI.toCode());
-        }
+        country.setCountryName(cname);
+        country.setCode(code);
         country.setServiceProvider(serviceProvider);
         serviceProvider.getCountryList().add(country);
         serviceProviderRepository1.save(serviceProvider);
-
         return serviceProvider;
     }
-
-
-    private boolean isValidCountryName(String countryName){
-        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("AUS")||countryName.equalsIgnoreCase("JPN")||countryName.equalsIgnoreCase("CHI")) {
-            return true;
-        }
-        else
-            return false;
-    }
 }
+//import com.driver.model.Admin;
+//import com.driver.model.Country;
+//import com.driver.model.CountryName;
+//import com.driver.model.ServiceProvider;
+//import com.driver.repository.AdminRepository;
+//import com.driver.repository.CountryRepository;
+//import com.driver.repository.ServiceProviderRepository;
+//import com.driver.services.AdminService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AdminServiceImpl implements AdminService {
+//    @Autowired
+//    AdminRepository adminRepository1;
+//
+//    @Autowired
+//    ServiceProviderRepository serviceProviderRepository1;
+//
+//    @Autowired
+//    CountryRepository countryRepository1;
+//
+//    @Override
+//    public Admin register(String username, String password) {
+//        Admin admin = new Admin(username,password);
+//
+//        adminRepository1.save(admin);
+//        return admin;
+//    }
+//
+//    @Override
+//    public Admin addServiceProvider(int adminId, String providerName) {
+//        Admin admin = adminRepository1.findById(adminId).get();
+//        ServiceProvider serviceProvider = new ServiceProvider(providerName);
+//
+//        serviceProvider.setAdmin(admin);
+//        admin.getServiceProviders().add(serviceProvider);
+//
+//        adminRepository1.save(admin);
+//        return admin;
+//    }
+//
+//    @Override
+//    public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+//        Country country = new Country();
+//        ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+//
+//        if(!isValidCountryName(countryName)){
+//            throw new Exception("Country not found");
+//        }
+//
+//        if (countryName.equalsIgnoreCase("IND")){
+//            country.setCountryName(CountryName.IND);
+//            country.setCode(CountryName.IND.toCode());
+//        }
+//        if (countryName.equalsIgnoreCase("USA")){
+//            country.setCountryName(CountryName.USA);
+//            country.setCode(CountryName.USA.toCode());
+//        }
+//        if (countryName.equalsIgnoreCase("AUS")){
+//            country.setCountryName(CountryName.AUS);
+//            country.setCode(CountryName.AUS.toCode());
+//        }
+//        if (countryName.equalsIgnoreCase("JPN")){
+//            country.setCountryName(CountryName.JPN);
+//            country.setCode(CountryName.JPN.toCode());
+//        }
+//        if (countryName.equalsIgnoreCase("CHI")){
+//            country.setCountryName(CountryName.CHI);
+//            country.setCode(CountryName.CHI.toCode());
+//        }
+//        country.setServiceProvider(serviceProvider);
+//        serviceProvider.getCountryList().add(country);
+//        serviceProviderRepository1.save(serviceProvider);
+//
+//        return serviceProvider;
+//    }
+//
+//
+//    private boolean isValidCountryName(String countryName){
+//        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("AUS")||countryName.equalsIgnoreCase("JPN")||countryName.equalsIgnoreCase("CHI")) {
+//            return true;
+//        }
+//        else
+//            return false;
+//    }
+//}
